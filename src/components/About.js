351d5c7dec23c1ed71331c8fc89f2675019cc5e1
“Aaron <github email address>
2021-03-15 19:23:52 -0400

    completed

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // if (!props.bio) return null;
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio && props.bio.length > 1 ? <p>{props.bio}</p> : null}
+      {/* <p>{props.bio}</p> */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
+      {/* <a href={props.links.github}>Github</a>
+      <a href={props.links.linkedin}>Linkedin</a> */}
     </div>
   );
 }
